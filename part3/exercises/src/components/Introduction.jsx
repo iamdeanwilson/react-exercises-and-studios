d81722f5e33f20b387a641530a37b03709810a55
Dean Wilson <Dean Wilson>
2023-11-19 17:21:35 -0600

    Completed React Part 3 Exercises

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Jiu Jitsu</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>You get to wear a real fun outfit called a "gi" that looks kinda like pajamas.</li>
+            <li>There's no striking, so you don't have to worry about getting punched or kicked!</li>
+            <li>When you tell your coworkers about it, they'll think you do karate.</li>
          </ol>
       </div>
    );
